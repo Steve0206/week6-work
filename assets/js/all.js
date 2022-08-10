2c98f3ccf50ec940d3a16bce8d44dc9fc98565ae
smalldot <z0976530159@gmail.com>
2022-08-10 17:38:57 +0800

    Update 2022-08-10T09:38:51.323Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -9,8 +9,11 @@ $('document').ready(function () {
   var swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    loop: false,
-    speed: 1000,
+    loop: true,
+    speed: 2000,
+    autoplay: {
+      delay: 2000
+    },
     effect: 'slide',
     // If we need pagination
     pagination: {
@@ -21,6 +24,18 @@ $('document').ready(function () {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
     }
-  });
+  }); //滑鼠移過去停止撥動
+
+  swiper.el.onmouseover = function () {
+    swiper.autoplay.stop();
+  }; //滑鼠移開自動撥放
+
+
+  swiper.el.onmouseout = function () {
+    swiper.autoplay.start();
+  };
+
+  var elem = document.querySelector('input[name="foo"]');
+  var datepicker = new Datepicker(elem, {});
 });
 //# sourceMappingURL=all.js.map
