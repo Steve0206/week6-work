936de83532339cbf35c84fb85305fee43a911041
smalldot <z0976530159@gmail.com>
2022-08-15 19:54:25 +0800

    fix

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -27,18 +27,10 @@ $('document').ready(function() {
 		},
 		
 	});
-		//滑鼠移過去停止撥動
-	swiper.el.onmouseover = function(){
-		swiper.autoplay.stop();
-	}
 
-	//滑鼠移開自動撥放
-	swiper.el.onmouseout = function(){
-		swiper.autoplay.start();
-	}
-	const elem = document.querySelector('input[name="foo"]');
-	const datepicker = new Datepicker(elem, {
-		
-	}); 
+	var date = document.querySelector('#date');
+	var datepicker = new Datepicker(date, {
+		language: 'zh-TW',
+	});
     
 });
